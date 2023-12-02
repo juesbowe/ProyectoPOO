02f9ee8822e1d2466449e9220345ad539e186165
Engrosal <151480848+Engrosal@users.noreply.github.com>
2023-12-01 19:08:49 -0500

    Update FichaComodin.java

--- src/main/java/ec/edu/espol/proyectopoo1/FichaComodin.java
+++ src/main/java/ec/edu/espol/proyectopoo1/FichaComodin.java
@@ -8,20 +8,25 @@ package ec.edu.espol.proyectopoo1;
  *
  * @author jose-
  */
+//Definicion de la clase FichaCOmodin que hereda de la clase Ficha
 public class FichaComodin extends Ficha{
     
+    // Constructor de la clase FichaComodin que inicializa los lados con valores de (-1,-1)
     public FichaComodin(int lado1, int lado2) {
         super(-1, -1);
     }
-    
+
+    // Metodo para establecer el valor del lado1 del comodin
     public void setLado1(int lado1){
         super.lado1=lado1;
     }
     
+    // Metodo para establecer el valor del lado2 del comodin
     public void setLado2(int lado2){
         super.lado2=lado2;
     }
     
+    // Metodo toString que devuelve una representacion en cadena del comodin, con asteriscos al inicio y al final
     @Override
     public String toString(){
         return "*"+super.toString()+"*";
